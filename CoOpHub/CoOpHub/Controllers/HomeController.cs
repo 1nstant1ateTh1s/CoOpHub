62fb91c7e063e5f91bc2295de8132b5a4a429777
Andrew Brown <ambrown@cityofchesapeake.net>
2018-06-28 14:49:17 -0400

    Add a name to host.

--- CoOpHub/CoOpHub/Controllers/HomeController.cs
+++ CoOpHub/CoOpHub/Controllers/HomeController.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CoOpHub.Models;
+using CoOpHub.ViewModels;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CoOpHub.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ApplicationDbContext _context;
+
+		public HomeController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
 		public ActionResult Index()
 		{
-			return View();
+			// Eagerly load upcoming co-op sessions
+			var upcomingCoops = _context.Coops
+				.Include(g => g.Host)	// include the related "Host" object
+				.Include(g => g.Game)	// include the related "Game" object
+				.Include(g => g.Game.Genre)		// include the related "Genre" object
+				.Where(g => g.DateTime > DateTime.Now);
+
+			// Create view model
+			var viewModel = new CoopsViewModel
+			{
+				UpcomingCoops = upcomingCoops,
+				ShowActions = User.Identity.IsAuthenticated,
+				Heading = "Upcoming Co-op sessions"
+			};
+
+			return View("Coops", viewModel);
 		}
 
 		public ActionResult About()
---
+++ CoOpHub/CoOpHub/ViewModels/CoopsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CoOpHub.Models;
+using System.Collections.Generic;
+
+namespace CoOpHub.ViewModels
+{
+	public class CoopsViewModel
+	{
+		public IEnumerable<Coop> UpcomingCoops { get; set; }
+		public bool ShowActions { get; set; }
+		public string Heading { get; set; }
+	}
+}
