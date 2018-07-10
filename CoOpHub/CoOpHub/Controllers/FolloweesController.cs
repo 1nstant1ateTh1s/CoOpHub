a5edec2eb7883c963eb0d5286adcb379a33d1da5
Andrew Brown <ambrown@cityofchesapeake.net>
2018-07-10 15:17:05 -0400

    Implement Who I'm Following.
    Related Work Items: #56

---
+++ CoOpHub/CoOpHub/Controllers/FolloweesController.cs
@@ -0,0 +1,29 @@
+﻿using CoOpHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CoOpHub.Controllers
+{
+	public class FolloweesController : Controller
+	{
+		private ApplicationDbContext _context;
+
+		public FolloweesController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		public ActionResult Index()
+		{
+			// Get hosts the current user is following
+			var userId = User.Identity.GetUserId();
+			var hosts = _context.Followings
+				.Where(f => f.FollowerId == userId)
+				.Select(f => f.Followee)
+				.ToList();
+
+			return View(hosts);
+		}
+	}
+}
