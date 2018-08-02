7e201a42ed754445413bad3c6be490df4cc8cf76
Andrew Brown <ambrown@cityofchesapeake.net>
2018-08-02 13:43:34 -0400

    Create new integration tests project.

---
+++ CoOpHub/CoOpHub.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Data.Entity.Migrations;
+
+namespace CoOpHub.IntegrationTests
+{
+	[SetUpFixture]
+	class GlobalSetUp
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			// Get Code-First configurations to bring integration testing database to latest version
+			var configuration = new CoOpHub.Migrations.Configuration();
+
+			// Migrate integration testing database to latest version
+			var migrator = new DbMigrator(configuration);
+			migrator.Update();
+		}
+	}
+}
---
+++ CoOpHub/CoOpHub.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoOpHub.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoOpHub.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9ad98a9-0eb1-4260-95f3-fd1ac958502c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
