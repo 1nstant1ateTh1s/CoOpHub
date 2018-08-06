ca0c019cd6f5155b09fab136c777b3e4ae65fb83
Andrew Brown <ambrown@cityofchesapeake.net>
2018-08-06 12:27:05 -0400

    Add add't. domain model unit tests to test notifications.

---
+++ CoOpHub/CoOpHub.Tests/Domain/Models/ApplicationUserTests.cs
@@ -0,0 +1,34 @@
+﻿using CoOpHub.Core.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace CoOpHub.Tests.Domain.Models
+{
+	[TestClass]
+	public class ApplicationUserTests
+	{
+		[TestMethod]
+		public void Notify_WhenCalled_ShouldAddTheNotification()
+		{
+			// Arrange
+			var user = new ApplicationUser();
+			var notification = Notification.CoopCanceled(new Coop());
+
+			// Act
+			user.Notify(notification);
+
+			// Assert
+			// We have 3 assertions here, but this does not mean this test method
+			// is violating the single responibility principle. These 3 assertions
+			// are highly related and we're logically verifying one fact: that
+			// the user object will have one UserNotification object in the right
+			// state (meaning its User and Notification properties are set properly).
+			user.UserNotifications.Count.Should().Be(1);
+
+			var userNotification = user.UserNotifications.First();
+			userNotification.Notification.Should().Be(notification);
+			userNotification.User.Should().Be(user);
+		}
+	}
+}
---
+++ CoOpHub/CoOpHub.Tests/Domain/Models/NotificationTests.cs
@@ -0,0 +1,31 @@
+﻿using CoOpHub.Core.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoOpHub.Tests.Domain.Models
+{
+	[TestClass]
+	public class NotificationTests
+	{
+		[TestMethod]
+		public void CoopCanceled_WhenCalled_ShouldReturnANotificationForACanceledCoop()
+		{
+			// Arrange
+			var coop = new Coop();
+
+			// Act
+			var notification = Notification.CoopCanceled(coop);
+
+			// Assert
+			// Again, here, we have two assertions, but that doesn't mean we're
+			// violating the single responsibility principle. We're verifying 
+			// one logical fact: that upon calling Notification.CoopCanceled()
+			// we'll get a notification object for the canceled co-op session. This notification
+			// object should be in the right state, meaning its type should be
+			// CoopCanceled and its coop should be the coop for each we created 
+			// the notification. 
+			notification.Type.Should().Be(NotificationType.CoopCanceled);
+			notification.Coop.Should().Be(coop);
+		}
+	}
+}
